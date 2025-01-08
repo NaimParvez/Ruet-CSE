cf600217d794a4be56dd13308bcf634a0d843b4c
NaimParvez <parveznaim0@gmail.com>
2025-01-08 08:49:40 +0600

    after 4th week

---
+++ AI/lab1.py
@@ -0,0 +1,10 @@
+#basic python syntax
+
+course='artificial intelligence'
+
+course=course.replace('artificial','machine')
+
+print(course)
+print(course.find('z'))
+print(course.isalpha())
+print(course.isalnum())
